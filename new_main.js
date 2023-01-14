be14be444f13b11cf48be6793b3f6c68eadd1d55
Nihira Golasangi <replynihira@gmail.com>
2023-01-14 17:36:46 +0530

    Update new_main.js

--- new_main.js
+++ new_main.js
@@ -14,9 +14,30 @@ var subpy = null;
 
 app.on('ready', function () {
   // call python?
-  subpy = require('child_process').spawn('python', ['./pose_backend.py']);
-  //var subpy = require('child_process').spawn('./dist/hello.exe');
+  // subpy = require('child_process').spawn('python', ['./pose_backend.py']);
+  var subpy = require('child_process').spawn('./dist/pose_backend.exe');
   var rq = require('request-promise');
+
+  // let backend;
+  // backend = path.join(process.cwd(), './dist/pose_backend.exe')
+  // var execfile = require('child_process').execFile;
+  // execfile(
+  //   backend,
+  //   {
+  //     windowsHide: true,
+  //   },
+  //   (err, stdout, stderr) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     if (stdout) {
+  //       console.log(stdout);
+  //     }
+  //     if (stderr) {
+  //       console.log(stderr);
+  //     }
+  //   }
+  // )
   var mainAddr = 'http://127.0.0.1:5000';
 
   var openWindow = function () {
@@ -48,7 +69,7 @@ app.on('ready', function () {
     attempts = attempts || 0;
 
     // maximum number of attempts
-    var maxAttempts = 10;
+    var maxAttempts = 20;
 
     rq(mainAddr)
       .then(function (htmlString) {
@@ -74,17 +95,50 @@ app.on('ready', function () {
 });
 
 
+
 app.on('window-all-closed', function () {
   //if (process.platform != 'darwin') {
   // app.quit();
   //}
-  try {
-    console.log('closing server')
-    subpy.stdin.write('\x03')
-  } catch (err) {
-    console.log(err);
-    console.log('subpy not started');
-    console.log('Cannot close server');
-  }
+  // try {
+  //         subpy.stdin.write('\x03')
+  //   } catch(err) {
+  //         console.log(err);
+  //         console.log('subpy not started');
+  //         console.log('Cannot close server');
+  //   }
+
+  // const { exec } = require('child_process');
+  // exec('taskkill /f /t /im app.exe', (err, stdout, stderr) => {
+  //  if (err) {
+  //   console.log(err)
+  //  return;
+  //  }
+  //  console.log(`stdout: ${stdout}`);
+  //  console.log(`stderr: ${stderr}`);
+  // });
+
+  // Declaring tree-kill
+
+  // var kill = require('tree-kill');
+  // try {
+  //   kill(subpy.pid);
+  // }
+  // catch (err) {
+  //   console.log(err);
+  //   console.log('subpy not started');
+  //   console.log('Cannot close server');
+  // }
+
+  const { exec } = require('child_process');
+  exec('taskkill /f /t /im pose_backend.exe', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+
   app.quit();
 });
