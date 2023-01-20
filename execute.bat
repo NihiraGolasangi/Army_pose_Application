d543d6bfa9cd0672baa43aeb6e617d8a316c9bf4
Atharva Parikh <54139853+AtharvaParikh@users.noreply.github.com>
2023-01-20 10:16:03 +0530

    added timeout for starting the server and async

--- execute.bat
+++ execute.bat
@@ -3,6 +3,18 @@ CALL env/Scripts/activate.bat
 REM for the below command to woek you need a python virtual environmen
 set FLASK_APP=pose_backend.py
 set FLASK_ENV=development
-flask run
+start flask run
 REM the below command will start a flask server on port 5000 on system default browser
-start http://127.0.0.1:5000/
+@REM start http://127.0.0.1:5000/
+
+:loop
+ping 127.0.0.1 -n 2 >nul
+IF %ERRORLEVEL% NEQ 0 (
+   echo Server not started yet, waiting for 10 seconds
+   timeout /t 10
+   goto loop
+) ELSE (
+   echo Server started
+@REM    start chrome http://localhost:8000
+    start http://localhost:5000/
+)
